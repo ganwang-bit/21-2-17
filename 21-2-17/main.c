0424ed577f8dec97ef4a8bc8bdca729a4cd5c3ce
ganwang <74584251+ganwang-bit@users.noreply.github.com>
2021-02-17 22:04:18 +0800

    21-2-17

---
+++ 21-2-17/main.c
@@ -0,0 +1,33 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//    printf("Hello world!\n");
+//
+//}//    return 0;
+//}
+//#include<conio.h>
+//#include<string.h>
+//#include<stdio.h>
+//int main()
+//{
+//    if(strpbrk("123456","61251"))
+//        printf("yes");
+//    else
+//        printf("no");
+//    return 0;
+//}
+//#include<string.h>
+//int main()
+//{
+//    strspn();
+//    strcspn();
+//    strpbrk();
+//    memcpy();
+//    memset();
+//    memmove();
+//    memchr();
+//    strtok();
+//    return 0;
+//}
